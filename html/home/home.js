9f5209d9a1efe5fa0e1e8c7f41b37452a3e2dc6c
thbts <1205552014@qq.com>
2019-04-26 17:59:21 +0800

    001

--- html/home/home.js
+++ html/home/home.js
@@ -1,25 +1,49 @@
 var data=[{
-	img:'',
-	name:'绿都澜湾1',
-	region:['住宅1','管城回族区'],
-	mianji:'建面72-143㎡',
+	img:'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/b51889744910df7979a2f672434da84e.jpg?thumb=1&w=720&h=360',
+	name:'郑地美景东望',
+	region:['住宅','郑东新区'],
+	mianji:'建面107-400㎡',
 	state:"在售",
-	features:['小户型1','绿化率高','车位充足','近地铁'],
-	price:'14000元/㎡'
+	features:['小户型','南北通透','配套成熟'],
+	price:'售价待定'
 },{
-	img:'',
-	name:'绿都澜湾2',
-	region:['住宅2','管城回族区'],
-	mianji:'建面72-143㎡',
+	img:'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/37dfdc929ee9a4313facb0b23ebcd721.jpg?thumb=1&w=720&h=360',
+	name:'正商善水上镜',
+	region:['郑东新区','郑东新区'],
+	mianji:'建面180-203㎡',
 	state:"待售",
-	features:['小户型2','绿化率高','车位充足','近地铁'],
+	features:['绿化率高','车位充足'],
+	price:'32000元/㎡'
+},{
+	img:'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/a0ff3dc30027f3b615bfe03f1d306ee5.jpg?thumb=1&w=720&h=360',
+	name:'世茂云尚城',
+	region:['住宅','管城回族区'],
+	mianji:'建面43-133㎡',
+	state:"售罄",
+	features:['小户型','绿化率高','车位充足','近地铁'],
 	price:'14000元/㎡'
 },{
-	img:'',
-	name:'绿都澜湾3',
-	region:['住宅3','管城回族区'],
-	mianji:'建面72-143㎡',
+	img:'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/2320573b3be643e29f5270a97e1a9c1d.jpg?thumb=1&w=720&h=360',
+	name:'亚新茉莉公馆',
+	region:['郑东新区','郑东新区'],
+	mianji:'建面130-220㎡',
 	state:"售罄",
-	features:['小户型3','绿化率高','车位充足','近地铁'],
-	price:'14000元/㎡'
+	features:['品牌房企','配套成熟','绿化率高'],
+	price:'37000元/㎡'
+},{
+	img:'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/b51889744910df7979a2f672434da84e.jpg?thumb=1&w=720&h=360',
+	name:'亚新茉莉公馆',
+	region:['郑东新区','郑东新区'],
+	mianji:'建面130-220㎡',
+	state:"售罄",
+	features:['品牌房企','配套成熟','绿化率高'],
+	price:'37000元/㎡'
+},{
+	img:'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/2320573b3be643e29f5270a97e1a9c1d.jpg?thumb=1&w=720&h=360',
+	name:'亚新茉莉公馆6',
+	region:['郑东新区','郑东新区'],
+	mianji:'建面130-220㎡',
+	state:"售罄",
+	features:['品牌房企','配套成熟','绿化率高'],
+	price:'37000元/㎡'
 }]
--- js/js.js
+++ js/js.js
@@ -1,11 +1,13 @@
 //css
+document.write('<link rel="stylesheet" type="text/css" href="../../icon/iconfont.css"/>');
 document.write('<link rel="stylesheet" type="text/css" href="../../css/swiper-3.4.2.min.css"/>');
-
-
+document.write('<link rel="stylesheet" type="text/css" href="../../css/jquery.mloading.css"/>');
 
 
 //js
 document.write('<script src="../../js/jquery.js"><\/script>');
+document.write('<script src="../../js/iscroll.js"><\/script>');
 document.write('<script src="../../js/flexible.js"><\/script>');
 document.write('<script src="../../js/ajax.js"><\/script>');
 document.write('<script src="../../js/swiper-3.4.2.jquery.min.js"></script>');
+document.write('<script src="../../js/jquery.mloading.js"></script>');
