faefd1f98e74dfa25b0373f9341b7f3beede0ccc
thbts <1205552014@qq.com>
2019-05-07 18:34:29 +0800

    000

--- js/js.js
+++ js/js.js
@@ -3,12 +3,10 @@ document.write('<link rel="stylesheet" type="text/css" href="../../css/base.css"
 document.write('<link rel="stylesheet" type="text/css" href="../../css/layouts.css"/>');
 document.write('<link rel="stylesheet" type="text/css" href="../../icon/iconfont.css"/>');
 document.write('<link rel="stylesheet" type="text/css" href="../../css/swiper-3.4.2.min.css"/>');
-document.write('<link rel="stylesheet" type="text/css" href="../../css/jquery.mloading.css"/>');
 
 
 //js
 document.write('<script src="../../js/jquery.js"><\/script>');
-document.write('<script src="../../js/iscroll.js"><\/script>');
 document.write('<script src="../../js/flexible.js"><\/script>');
 document.write('<script src="../../js/ajax.js"><\/script>');
 document.write('<script src="../../js/swiper-3.4.2.jquery.min.js"></script>');
