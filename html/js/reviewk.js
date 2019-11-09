8be9e0e11c727b2f0190077c108685571e5e6898
thbts <1205552014@qq.com>
2019-11-09 15:16:17 +0800

    022

---
+++ html/js/reviewk.js
@@ -0,0 +1,37 @@
+$(function(){
+	/**
+	 * 登录状态
+	 */
+	var yz_logins=JSON.parse(localStorage.getItem('yz_logins'));//转为对象
+	if(!yz_logins){
+		location.href="home.html";
+	}
+	//输入数量限制
+	var len=0,total=200;
+	$(".review_box>div").html(len+'/'+total);
+	$('.puba').on('input propertychange', function (e){
+	    var texts=$(this).val();
+		if(texts.length>total){
+			len=total;
+		}else{
+			len=texts.length;
+		}
+	    $(".review_box>div").html(len+'/'+total);
+	    if(len==0){
+	    	$(".rights").css({"color":"#333333"});
+	    }else{
+	    	$(".rights").css({"color":"#0099ff"});
+	    }
+	});
+	/**
+	 * 发表
+	 */
+	$(".rights").click(function(){
+		if(len==0){
+			alert("发表内容不可为空3");
+		}else{
+			alert("提交成功3")
+		}
+		console.log($(".puba").val());
+	})
+})
